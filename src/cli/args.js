b81a555a612ab4bd64dfbd84c57157aa9ed3d48d
Margarita Zoryna <Margarita_Zoryna@epam.com>
2024-01-27 19:40:41 +0300

    feat: Command line interface(src/cli)

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,16 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+  const parsedArgs = {};
+
+  for (let i = 0; i < args.length; i += 2) {
+    const propertyName = args[i].slice(2);
+    const propertyValue = args[i + 1];
+    parsedArgs[propertyName] = propertyValue;
+  }
+
+  Object.entries(parsedArgs).forEach(([propertyName, propertyValue]) => {
+    console.log(`${propertyName} is ${propertyValue}`);
+  });
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
-    // Write your code here 
+  const prefix = "RSS_";
+  const envVariables = Object.keys(process.env)
+    .filter((key) => {
+      console.log("key", key);
+      return key.startsWith(prefix);
+    })
+    .map((key) => `RSS_${key.slice(prefix.length)}=${process.env[key]}`)
+    .join("; ");
+
+  console.log(envVariables);
 };
 
 parseEnv();
