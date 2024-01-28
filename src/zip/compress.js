0bdd06ad8441812da92e8917b4819e36d62b97a7
Margarita Zoryna <Margarita_Zoryna@epam.com>
2024-01-28 14:42:17 +0300

    feat:Zlib (src/zip)

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,27 @@
+import fs from "fs";
+import zlib from "zlib";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
 const compress = async () => {
-    // Write your code here 
+  const currentFilePath = fileURLToPath(import.meta.url);
+  const currentFolderPath = dirname(currentFilePath);
+  const filePath = join(currentFolderPath, "files", "fileToCompress.txt");
+  const zipPath = join(currentFolderPath, "files", "archive.gz");
+
+  const readStream = fs.createReadStream(filePath);
+  const writeStream = fs.createWriteStream(zipPath);
+  const gzipStream = zlib.createGzip();
+
+  readStream.pipe(gzipStream).pipe(writeStream);
+
+  writeStream.on("finish", () => {
+    console.log("Compression completed.");
+  });
+
+  writeStream.on("error", (error) => {
+    console.error("An error occurred:", error);
+  });
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,27 @@
+import fs from "fs";
+import zlib from "zlib";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
 const decompress = async () => {
-    // Write your code here 
+  const currentFilePath = fileURLToPath(import.meta.url);
+  const currentFolderPath = dirname(currentFilePath);
+  const filePath = join(currentFolderPath, "files", "fileToCompress.txt");
+  const archivePath = join(currentFolderPath, "files", "archive.gz");
+
+  const readStream = fs.createReadStream(archivePath);
+  const writeStream = fs.createWriteStream(filePath);
+  const gunzipStream = zlib.createGunzip();
+
+  readStream.pipe(gunzipStream).pipe(writeStream);
+
+  writeStream.on("finish", () => {
+    console.log("Decompression completed.");
+  });
+
+  writeStream.on("error", (error) => {
+    console.error("An error occurred:", error);
+  });
 };
 
 await decompress();
