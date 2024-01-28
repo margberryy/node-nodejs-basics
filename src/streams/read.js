600fec10267ab8dcc9e775551066e9636ce36cfd
Margarita Zoryna <Margarita_Zoryna@epam.com>
2024-01-28 14:32:33 +0300

    feat: Streams (src/streams)

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,25 @@
+import fs from "fs";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
 const read = async () => {
-    // Write your code here 
+  const currentFilePath = fileURLToPath(import.meta.url);
+  const currentFolderPath = dirname(currentFilePath);
+  const filePath = `${currentFolderPath}/files/fileToRead.txt`;
+
+  const stream = fs.createReadStream(filePath);
+
+  stream.on("data", (chunk) => {
+    process.stdout.write(chunk);
+  });
+
+  stream.on("end", () => {
+    console.log("\nFile reading completed.");
+  });
+
+  stream.on("error", (error) => {
+    console.error("An error occurred while reading the file:", error);
+  });
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,22 @@
+import fs from "fs";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
 const write = async () => {
-    // Write your code here 
+  const currentFilePath = fileURLToPath(import.meta.url);
+  const currentFolderPath = dirname(currentFilePath);
+  const filePath = join(currentFolderPath, "files", "fileToWrite.txt");
+  const stream = fs.createWriteStream(filePath);
+  process.stdin.pipe(stream);
+  console.log("Please enter some input:");
+
+  process.stdin.on("end", () => {
+    console.log("Data writing completed.");
+  });
+
+  stream.on("error", (error) => {
+    console.error("An error occurred:", error);
+  });
 };
 
 await write();
