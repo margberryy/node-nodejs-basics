535fbc2216f1e8b5ad185c9e215ee3648437193c
Margarita Zoryna <Margarita_Zoryna@epam.com>
2024-01-28 13:46:42 +0300

    feat: Hash (src/hash)

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,28 @@
+import fs from "fs";
+import crypto from "crypto";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const currentFilePath = fileURLToPath(import.meta.url);
+  const currentFolderPath = dirname(currentFilePath);
+  const filePath = `${currentFolderPath}/files/fileToCalculateHashFor.txt`;
+
+  const stream = fs.createReadStream(filePath);
+  const hash = crypto.createHash("sha256");
+
+  stream.on("data", (chunk) => {
+    hash.update(chunk);
+  });
+
+  stream.on("end", () => {
+    const calculatedHash = hash.digest("hex");
+    console.log(calculatedHash);
+  });
+
+  stream.on("error", (error) => {
+    console.error(error);
+  });
 };
 
 await calculateHash();
